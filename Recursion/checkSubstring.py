4f209fff5683d35a0cba10dd0cb1b4a8c9cadd0c
Shubham Goyal <sgoyal08@outlook.com>
2024-05-15 09:45:34 +0530

    create checkSubstring using recursion

---
+++ Recursion/checkSubstring.py
@@ -0,0 +1,25 @@
+# Given two string s and t, write a function to check if s contains all characters of t (in the same order as they are in string t).
+
+# Return true or false.
+
+# Do it recursively.
+
+# E.g. : s = “abchjsgsuohhdhyrikkknddg” contains all characters of t=”coding” in the same order. So function will return true.
+def contains(s,t):
+    if not t:
+        return True
+    if not s:
+        return False
+    if s[0] == t[0]:
+        return contains(s[1:], t[1:])
+    else:
+        return contains(s[1:], t)
+    
+s = input()
+t = input()
+
+ans = contains(s,t)
+if ans is True:
+    print('true')
+else:
+    print('false')
