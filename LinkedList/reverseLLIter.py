51a6ed03a3ac3988dfe693eb8c9ca8969786fb89
Shubham Goyal <sgoyal08@outlook.com>
2024-05-20 10:59:22 +0530

    create reverseLLIter.py

---
+++ LinkedList/reverseLLIter.py
@@ -0,0 +1,51 @@
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+def takeInput():
+        inputList = [int(ele) for ele in input().split()]
+        
+        head=None
+        tail=None
+        for currData in inputList:
+            if currData==-1:
+                break
+             
+            new_node=Node(currData)
+            if head==None:
+                head=new_node
+                tail=new_node
+            else:
+                tail.next=new_node
+                tail=new_node
+        return head
+def length(head): 
+
+
+    count=0
+    while head!=None or head==-1:
+          count+=1
+          head=head.next
+    return count
+def printll(head):
+    while head != None:
+          print(head.data," ->",end=' ')
+          head=head.next
+    print("None")
+def reverseLLIter(head):
+    curr=head
+    prev=None
+    while curr is not None:
+        next=curr.next
+        curr.next=prev
+        prev=curr
+        curr=next
+    head=prev
+    
+    return head
+    
+head=takeInput()
+printll(head)
+head=reverseLLIter(head)
+printll(head)
