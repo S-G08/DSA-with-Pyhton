1402681270cc8c744be9f19ed0e655efc8c83906
Shubham Goyal <sgoyal08@outlook.com>
2024-05-20 10:31:59 +0530

    create reverseLLRec.py

---
+++ LinkedList/reverseLLRec.py
@@ -0,0 +1,49 @@
+# Given a singly linked list of integers, reverse it using recursion and return the head to the modified list. You have to do this in O(N) time complexity where N is the size of the linked list.
+
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+def takeInput():
+        inputList = [int(ele) for ele in input().split()]
+        
+        head=None
+        tail=None
+        for currData in inputList:
+            if currData==-1:
+                break
+             
+            new_node=Node(currData)
+            if head==None:
+                head=new_node
+                tail=new_node
+            else:
+                tail.next=new_node
+                tail=new_node
+        return head
+def length(head): 
+
+
+    count=0
+    while head!=None or head==-1:
+          count+=1
+          head=head.next
+    return count
+def printll(head):
+    while head != None:
+          print(head.data," ->",end=' ')
+          head=head.next
+    print("None")
+def reverseLL(head):
+    if head is None or head.next is None:
+        return head,head
+    smallhead,smalltail=reverseLL(head.next)
+    smalltail.next=head
+    head.next=None
+    return smallhead,head
+    
+head=takeInput()
+printll(head)
+head,tail=reverseLL(head)
+printll(head)
