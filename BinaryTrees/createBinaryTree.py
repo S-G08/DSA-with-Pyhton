80fdbd610084fb8533138ca02e59e8de0fd106e7
Shubham Goyal <sgoyal08@outlook.com>
2024-05-27 10:36:52 +0530

    create node and print tree function

---
+++ BinaryTrees/createBinaryTree.py
@@ -0,0 +1,24 @@
+class BinaryTree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+def printTree(root):
+    if root == None:
+        return
+    print(root.data,end=":")
+    if root.left != None:
+        print("L",root.left.data,end=",")
+    if root.right != None:
+        print("R",root.right.data,end='')
+    print()
+    printTree(root.left)
+    printTree(root.right)
+
+
+btn1=BinaryTree(1)
+btn2=BinaryTree(2)
+btn3=BinaryTree(3)
+btn1.left=btn2
+btn1.right=btn3
+printTree(btn1)
