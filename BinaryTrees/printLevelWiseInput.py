275dee665465685f33b0e86bb5843feb4ac99bf7
Shubham Goyal <sgoyal08@outlook.com>
2024-05-31 14:05:39 +0530

    create ConstructTreeFromPreorderAndInorder.py

---
+++ BinaryTrees/printLevelWiseInput.py
@@ -0,0 +1,54 @@
+import queue
+class BinaryTree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+def takeLevelWiseInput():
+    q=queue.Queue()
+    print("Enter root data")
+    rootdata=int(input())
+    if(rootdata == -1):
+        return None
+    root=BinaryTree(rootdata)
+    q.put(root)
+    while (not(q.empty())):
+        current_node=q.get()
+        print("Enter left child of ",current_node.data)
+        leftnode_data=int(input())
+        if(leftnode_data!=-1):
+            leftnode=BinaryTree(leftnode_data)
+            current_node.left=leftnode
+            q.put(leftnode)
+        
+        print("Enter right child of ",current_node.data)
+        rightnode_data=int(input())
+        if(rightnode_data!=-1):
+            rightnode=BinaryTree(rightnode_data)
+            current_node.right=rightnode
+            q.put(rightnode)
+    return root
+def printLevelWise(root):
+    q=queue.Queue()
+    q.put(root)
+    while (not(q.empty())):
+        current=q.get()
+        print(current.data,end=':')
+        if(current.left ==None):
+            print("L:-1",end=',')
+        else:
+            print("L:",end='')
+            print(current.left.data,end=',')
+        if(current.right==None):
+            print("R:-1",end="")       
+        else:
+            print("R:",end='')
+            print(current.right.data,end="")
+        print()
+        if(current.left !=None):
+            q.put(current.left)
+        if(current.right!= None):
+            q.put(current.right)
+
+root=takeLevelWiseInput()
+printLevelWise(root)
