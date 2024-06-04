8547de0e78dca82db47cd5d4e0bff057982f1634
Shubham Goyal <sgoyal08@outlook.com>
2024-06-04 15:29:39 +0530

    create HeightOfGT.py

---
+++ GenericTrees/HeightOfGT.py
@@ -0,0 +1,39 @@
+import sys
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+    def __str__(self):
+        return str(self.data)
+
+def height(root):
+    if root==None:
+        return 0
+    h=0
+    for child in root.children:
+        ch=height(child)
+        h=max(h,ch)
+    
+    return h+1
+
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+sys.setrecursionlimit(10**6)
+arr = list(int(x) for x in input().strip().split(' '))
+tree = createLevelWiseTree(arr)
+print(height(tree))
