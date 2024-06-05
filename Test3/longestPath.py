5b073f02a63fb01ffbab964db2c33c73588bff5b
Shubham Goyal <sgoyal08@outlook.com>
2024-06-05 10:02:39 +0530

    create longestPath.py

---
+++ Test3/longestPath.py
@@ -0,0 +1,56 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def longestPath(root):
+    if root==None:
+        return []
+    rl=longestPath(root.right)
+    ll=longestPath(root.left)
+    if len(ll)>len(rl):
+        ll.append(root.data)
+    else:
+        rl.append(root.data)
+    
+    if len(ll)>len(rl):
+        return ll
+    else:
+        return rl
+    
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+path = longestPath(root)
+for ele in path:
+    print(ele)
