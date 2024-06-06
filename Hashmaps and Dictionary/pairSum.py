72e60b7f790b491fb685da3411d69cc8e1a0cb9f
Shubham Goyal <sgoyal08@outlook.com>
2024-06-06 12:57:41 +0530

    create pairSum.py

---
+++ Hashmaps and Dictionary/pairSum.py
@@ -0,0 +1,14 @@
+def pairSum(arr,target):
+    freq=dict()
+    ans=0
+    for num in arr:
+        expect=target-num
+        ans += freq.get(expect, 0)
+        if freq.get(num,0):
+            freq[num]+=1
+        else:
+            freq[num]=1
+    return ans
+
+ar=[int(x) for x in input().split()]
+print(pairSum(ar,4))
