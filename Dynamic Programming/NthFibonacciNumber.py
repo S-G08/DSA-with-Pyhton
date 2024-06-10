4c8241377db71c67b2f524ae64fb3532cf21f07c
Shubham Goyal <sgoyal08@outlook.com>
2024-06-10 12:31:47 +0530

    add better solution for space complexity

--- Dynamic Programming/NthFibonacciNumber.py
+++ Dynamic Programming/NthFibonacciNumber.py
@@ -7,12 +7,21 @@ def fibRec(n):
     return mem[n]
 
 def fibIte(n):
-    dp=[-1]*(n+1)
-    dp[0]=0
-    dp[1]=1
+    # dp=[-1]*(n+1)
+    # dp[0]=0
+    # dp[1]=1
+    # for i in range(2,n+1):
+    #     dp[i]=dp[i-1]+dp[i-2]
+    # return dp[n]
+    ## Space optimizing code
+    a=0
+    b=1
+    c=0
     for i in range(2,n+1):
-        dp[i]=dp[i-1]+dp[i-2]
-    return dp[n]
+        c=a+b
+        a=b
+        b=c
+    return c
 
 n=int(input())
 mem=[-1] * (n+1)
