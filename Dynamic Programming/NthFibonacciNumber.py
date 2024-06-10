f545ab59c30c9554d59463b00310b8ac88ee18ec
Shubham Goyal <sgoyal08@outlook.com>
2024-06-10 12:28:07 +0530

    add iterative approach

---
+++ Dynamic Programming/NthFibonacciNumber.py
@@ -0,0 +1,20 @@
+def fibRec(n):
+    if n==0 or n==1:
+        return n
+    elif mem[n] != -1:
+        return mem[n]
+    mem[n]=fibRec(n-1) + fibRec(n-2)
+    return mem[n]
+
+def fibIte(n):
+    dp=[-1]*(n+1)
+    dp[0]=0
+    dp[1]=1
+    for i in range(2,n+1):
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[n]
+
+n=int(input())
+mem=[-1] * (n+1)
+print("Recursively:",fibRec(n))
+print("Iteratively:",fibIte(n))
