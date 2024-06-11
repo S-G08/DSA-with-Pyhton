fdc16c271809eb0c46deba85a4cc321d1e742db9
Shubham Goyal <sgoyal08@outlook.com>
2024-06-11 11:40:24 +0530

    create rodCuttingProblem.py

---
+++ Dynamic Programming/rodCuttingProblem.py
@@ -0,0 +1,30 @@
+from sys import stdin
+import sys
+INT_MIN = -32767
+def cutRod(price, n):
+    val = [0 for x in range(n+1)]
+    val[0] = 0
+    for i in range(1, n+1):
+        max_val = INT_MIN
+        for j in range(i):
+             max_val = max(max_val, price[j] + val[i-j-1])
+        val[i] = max_val
+
+    return val[n]
+
+
+# Taking input using fast I/O.
+def takeInput():
+    n = int(input())
+
+    price = list(map(int, input().strip().split(" ")))
+
+    return price, n
+
+
+# Main.
+t = int(input())
+while t:
+    price, n = takeInput()
+    print(cutRod(price, n))
+    t = t-1
