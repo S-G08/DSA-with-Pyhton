5c63ae1065df97ca1db45b261bf4e31ee4a05f5b
Shubham Goyal <sgoyal08@outlook.com>
2024-06-14 10:11:27 +0530

    create arraySubsets.py

---
+++ Recursion3/arraySubsets.py
@@ -0,0 +1,20 @@
+def calcSubset(A, res, subset, index):
+    res.append(subset[:])
+    for i in range(index, len(A)):
+        subset.append(A[i])
+        calcSubset(A, res, subset, i + 1)
+        subset.pop()
+
+
+def subsets(A):
+    subset = []
+    res = []
+    index = 0
+    calcSubset(A, res, subset, index)
+    return res
+
+n=int(input())
+arr=[int(i) for i in input().split()]
+ans = subsets(arr)
+for ele in ans:
+    print(*ele)
