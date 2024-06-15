d3d8774d9edb3e6693c5eb8ffd69aea00a9fb1fc
Shubham Goyal <sgoyal08@outlook.com>
2024-06-15 13:03:04 +0530

    create sudoku.py

---
+++ Backtracking/sudoku.py
@@ -0,0 +1,50 @@
+def presentInRow(board,row,num):
+    for j in range(9):
+        if board[row][j] == num:
+            return True
+    return False
+
+def presentInCol(board,col,num):
+    for j in range(9):
+        if board[j][col] == num:
+            return True
+    return False
+
+def presentInBox(board,row,col,num):
+    for i in range(row,row+3):
+        for j in range(col,col+3):
+            if board[i][j] == num:
+                return True
+    return False
+
+def isPossible(board,row,col,num):
+    if(presentInRow(board,row,num)):
+        return False
+    if(presentInCol(board,col,num)):
+        return False
+    if(presentInBox(board,row-(row%3),col-(col%3),num)):
+        return False
+    return True
+def isItSudoku(board):
+    row = -1
+    col = -1
+    flag = False
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] is 0:
+                row = i
+                col = j
+                flag = True
+                break
+        if flag is True:
+            break
+    if row == -1:
+        return True
+    for num in range(1,10):
+        if(isPossible(board,row,col,num)):
+            board[row][col] = num
+            if(isItSudoku(board) is True):
+                return True
+            board[row][col] = 0
+            
+    return False
