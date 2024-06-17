2652fdd6d76ea790fb5c58b9d0e324611945387d
Shubham Goyal <sgoyal08@outlook.com>
2024-06-17 10:17:25 +0530

    add function for disconnected graphs

--- Graphs/Graphs1/DepthFirstSearch.py
+++ Graphs/Graphs1/DepthFirstSearch.py
@@ -2,6 +2,7 @@ class Graph:
     def __init__(self,n) -> None:
         self.graph=[]
         self.visited=[]
+        self.n=n
         for _ in range(n+1):
             self.graph.append([])
             self.visited.append(False)
@@ -11,18 +12,30 @@ class Graph:
     def print(self):
         print(self.graph)
 
+    def clear(self):
+        for i in range(len(self.visited)):
+            self.visited[i]=False
+            
+
     def dfs(self,s):
         self.visited[s]=True
         print("Visited:",s)
         for v in self.graph[s]:
             if not self.visited[v]:               
                 self.dfs(v)
-
-g=Graph(5)
+    
+    def _dfs(self,s):
+        self.clear()
+        for i in range(1,self.n+1):
+            if not self.visited[i]:
+                self.dfs(i)
+g=Graph(7)  
 g.add_edge(1,2)
 g.add_edge(1,3)
 g.add_edge(2,4)
 g.add_edge(3,4)
 g.add_edge(3,5)
-g.dfs(1)
+g.add_edge(6,7)
+
+g._dfs(1)
 
